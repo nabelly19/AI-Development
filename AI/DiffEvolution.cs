0c97ac73599febcbdc41d740046a3ea9383dccd2
nabelly <anabellymontibeller@gmail.com>
2024-02-26 16:57:33 -0300

    changing generation

--- AI/DiffEvolution.cs
+++ AI/DiffEvolution.cs
@@ -7,44 +7,62 @@ namespace AIContinuous;
 public class DiffEvolution
 {
     protected Func<double[], double> Fitness { get; }
+    protected Func<double[], double> Restriction { get; }
     protected int Dimension { get; set; }
+    protected double MutationMin { get; set; }
+    protected double MutationMax { get; set; }
+    protected double Recombination { get; set; }
     protected int Npop { get; set; }
     protected List<double[]> Individuals { get; set; }
     protected List<double[]> Bounds { get; set; }
+    private double[] IndividualsRestriction { get; set; }
+     private double[] IndividualsFitness { get; set; }
     protected int BestIndividualIndex { get; set; }
-    protected double BestIndividualFitness { get; set; } = double.MaxValue;
 
     public DiffEvolution(
         Func<double[], double> fitness,
         List<double[]> bounds,
-        int npop
+        int npop,
+        Func<double[], double> restriction,
+        double mutationMin = 0.5,
+        double mutationMax = 0.9,
+        double recombination = 0.8
+
     )
     {
         this.Fitness = fitness;
         this.Dimension = bounds.Count;
         this.Npop = npop;
-        Individuals = new List<double[]>(Npop);
+        this.Individuals = new List<double[]>(Npop);
         this.Bounds = bounds;
+        this.MutationMin = mutationMin;
+        this.MutationMax = mutationMax;
+        this.Recombination = recombination;
+        this.Restriction = restriction;
+        this.IndividualsRestriction = new double[Npop];
+        this.IndividualsFitness = new double[Npop];
 
     }
 
     private void GeneratePopulation()
     {
-
         var dimension = Dimension;
         for (int i = 0; i < Npop; i++)
         {
-            Individuals[i] = new double[Dimension];
+            Individuals.Add(new double[dimension]);
             for (int j = 0; j < Dimension; j++)
             {
-                Individuals[i][j] = Utils.Rescale(Random.Shared.NextDouble(), Bounds[j][0], Bounds[j][0]);
+                Individuals[i][j] = Utils.Rescale(Random.Shared.NextDouble(), Bounds[j][0], Bounds[j][1]);
             }
+            IndividualsRestriction[i] = Restriction(Individuals[i]);
+
+            IndividualsFitness[i] =  IndividualsRestriction[i] <= 0.0 ? Fitness(Individuals[i]) : double.MaxValue;
         }
     }
 
     private void FindBestIndividual()
     {
-        var fitnessBest = BestIndividualFitness;
+        var fitnessBest = IndividualsFitness[BestIndividualIndex];
         for (int i = 0; i < Npop; i++)
         {
             var fitnessCurrent = Fitness(Individuals[i]);
@@ -55,24 +73,76 @@ public class DiffEvolution
                 fitnessBest = fitnessCurrent;
             }
         }
-        BestIndividualFitness = fitnessBest;
+        IndividualsFitness[BestIndividualIndex] = fitnessBest;
     }
 
-    private double[] Mutate(double[] individual)
+    private double[] Mutate(int index)
     {
         var newIndividual = new double[Dimension];
-        newIndividual = Individuals[BestIndividualIndex];
-        for(int i = 0; i < Dimension; i ++)
+
+        int individualRand1;
+        int individualRand2;
+
+        do individualRand1 = Random.Shared.Next(Npop);
+        while (individualRand1 == index);
+
+        do individualRand2 = Random.Shared.Next(Npop);
+        while (individualRand2 == individualRand1);
+
+
+        newIndividual = (double[])Individuals[BestIndividualIndex].Clone();
+        for (int i = 0; i < Dimension; i++)
         {
-            newIndividual[i] += Individuals[Random.Shared.Next()][i] - Individuals[Random.Shared.Next(Npop)][i];
+            newIndividual[i] += Utils.Rescale(Random.Shared.NextDouble(), MutationMin, MutationMax) * (Individuals[individualRand1][i] - Individuals[individualRand2][i]);
         }
 
-        return individual;
+        return newIndividual;
     }
-    public double[] Optimize()
+
+    protected double[] Crossover(int index)
+    {
+        var trial = Mutate(index);
+        var trial2 = (double[])Individuals[index].Clone();
+
+        for (int i = 0; i < Dimension; i++)
+        {
+            // if(!((Random.Shared.NextDouble() < Recombination) || (i == Random.Shared.Next(Dimension))))
+            if ((Random.Shared.NextDouble() > Recombination) && (i != Random.Shared.Next(Dimension)))
+                trial2[i] = trial[i];
+        }
+
+        return trial2;
+    }
+
+    protected void Iterate()
+    {
+        for (int i = 0; i < Npop; i++)
+        {
+            var trial = Crossover(i);
+
+            var restTrial = Restriction(trial);
+            var restIndividual = Restriction(Individuals[i]);
+            var fitnessTrial = restTrial <= 0.0 ? Fitness(trial) : double.MaxValue;
+
+            if ((restIndividual > 0.0 && (restTrial < restIndividual))
+                || (restTrial <= 0.0 && restIndividual > 0.0)
+                || (restTrial <= 0.0 && fitnessTrial < IndividualsFitness[i]))
+            {
+                Individuals[i] = trial;
+                IndividualsRestriction[i] =restTrial;
+                IndividualsFitness[i] = fitnessTrial;
+
+            }
+        }
+        FindBestIndividual();
+    }
+
+    public double[] Optimize(int n)
     {
         GeneratePopulation();
-        FindBestIndividual();
+
+        for (int i = 0; i < n; i++)
+            Iterate();
 
         return Individuals[BestIndividualIndex];
     }
--- AI/Program.cs
+++ AI/Program.cs
@@ -1,46 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using AIContinuous;
 
-double FuncA(double x){ return x + 1; }
+// double[] sol;
+// var num = new double[] {10,10};
 
-double MyFunction(double x) { return x * x; }
+List<double[]> bounds = new() {
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+    new double[] {-10.0, 10.0},
+};
 
-double MyDer(double x) { return 2.00 * x; }
+double Restriction(double[] x)
+{
+    return -1.0;
+}
 
-double MySqrt(double x) { return (Math.Sqrt(Math.Abs(x)) - 5) * x + 10; }
-
-double MyNewDer(double x) { return (1 / (2 * Math.Sqrt(Math.Abs(x))) - 5) * x + Math.Sqrt(Math.Abs(x));}
-
-double Function(double x ) { return Math.Pow(x - 1, 2) + Math.Sin(Math.Pow(x,3));}
-
-// double GradientFunction(double[] x) { return Math.Pow(x[0],2) + Math.Pow(x[1],2); }
-
-double GradientFunction(double[] x) { return Math.Pow(x[0] + 2.00 * x[1],2) + Math.Pow( 2.00 * x[0] + x[1] - 5, 2); }
-
-double RosenBrockFunction(double[] x) {
+double RosenBrockFunction(double[] x)
+{
 
     double hellyeah = 0;
     var n = x.Length;
     int ni = n - 1;
-    for( int i = 0; i < ni; i ++)
+    for (int i = 0; i < ni; i++)
     {
-        hellyeah += 100 * (x[i+1] - x[0] * x[0] ) * (x[i+1] - x[0] * x[0] ) + (1 - x[i]) * (1 - x[i]);
+        hellyeah += 100 * (x[i + 1] - x[0] * x[0]) * (x[i + 1] - x[0] * x[0]) + (1 - x[i]) * (1 - x[i]);
     }
     return hellyeah;
 }
 
-double[] sol;
-
 var date = DateTime.Now;
-date = DateTime.Now;
 
-var num = new double[] {10,10};
-double lr;
-double atol;
+var diffEvolution = new DiffEvolution(RosenBrockFunction, bounds, 100, Restriction);
+var res = diffEvolution.Optimize(1000);
 
 date = DateTime.Now;
-sol = Optmize.GradientDescent(RosenBrockFunction, num, lr = 1e-5, atol = 1e-9);
-Console.WriteLine($"Solution: {sol[0]} {sol[1]} | Time: {(DateTime.Now - date).TotalMilliseconds}");
+
+Console.WriteLine($"Solution: {res[0]} {res[1]} | Time: {(DateTime.Now - date).TotalMilliseconds}");
 
 
 
